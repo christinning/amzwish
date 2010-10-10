76308921be57d4491bead634844eb2f8e2abcec3
Chris Tinning <chris.tinning@gmail.com>
2010-10-10 16:09:17 +0100

    tidies up tests

--- spec/amzwish/wishlist_spec.rb
+++ spec/amzwish/wishlist_spec.rb
@@ -3,57 +3,49 @@ require 'open-uri'
 
 module Amzwish
   describe Wishlist do
-    context "empty wishlist" do
+    context "empty wishlist" do 
+      let(:mock_wrapper){ mock_website_wrapper( %w{ empty.html } ) }
+      let(:fixture){ Wishlist.new("address@email.com", mock_wrapper) }
       it "returns an empty list of books" do
-        mock_finder = mock(Services::WebsiteWrapper)
-        mock_finder.should_receive(:find_for).with("address@email.com").and_return([{:id=>"WISHLIST-ID"}])
-        empty_page = open(File.join(PROJECT_DIR, "samples","uk","empty.html")).read
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 1).and_return(empty_page)
-        fixture = Wishlist.new("address@email.com", mock_finder)
         fixture.books.size.should == 0
       end
     end
     
     context "single item wishlist" do
+      let(:mock_wrapper){ mock_website_wrapper( %w{ single-item.html } ) }
+      let(:fixture){ Wishlist.new("address@email.com", mock_wrapper) }
       it "returns a list of books" do
-        mock_finder = mock(Services::WebsiteWrapper)
-        mock_finder.should_receive(:find_for).with("address@email.com").and_return([{:id=>"WISHLIST-ID"}])
-        page = open(File.join(PROJECT_DIR, "samples","uk","single-item.html")).read
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 1).and_return(page)
-        fixture = Wishlist.new("address@email.com", mock_finder)
         books = fixture.books
         books.size.should == 1
         books[0].title.should == "Language Myths"     
       end
       
       it "should be enumerable" do
-        mock_finder = mock(Services::WebsiteWrapper)
-        mock_finder.should_receive(:find_for).with("address@email.com").and_return([{:id=>"WISHLIST-ID"}])
-        page = open(File.join(PROJECT_DIR, "samples","uk","single-item.html")).read
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 1).and_return(page)
-        fixture = Wishlist.new("address@email.com", mock_finder)
         books = []
         fixture.each_book{|b| books << b}
         books.size.should == 1
-        
       end 
     end
     
     context "multipage wishlist" do
+      let(:mock_wrapper){ mock_website_wrapper(%w{ multipage-page1.html multipage-page2.html multipage-page3.html multipage-page4.html}) }
+      let(:fixture){ Wishlist.new("address@email.com", mock_wrapper) }
       it "returns a list of books" do
-        mock_finder = mock(Services::WebsiteWrapper)
-        mock_finder.should_receive(:find_for).with("address@email.com").and_return([{:id=>"WISHLIST-ID"}]) 
-        page1 = open(File.join(PROJECT_DIR, "samples","uk","multipage-page1.html")).read
-        page2 = open(File.join(PROJECT_DIR, "samples","uk","multipage-page2.html")).read
-        page3 = open(File.join(PROJECT_DIR, "samples","uk","multipage-page3.html")).read
-        page4 = open(File.join(PROJECT_DIR, "samples","uk","multipage-page4.html")).read
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 1).and_return(page1)
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 2).and_return(page2)
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 3).and_return(page3)
-        mock_finder.should_receive(:get_page).with("WISHLIST-ID", 4).and_return(page4)
-        fixture = Wishlist.new("address@email.com", mock_finder)
+        fixture = Wishlist.new("address@email.com", mock_wrapper)
         fixture.books.size.should be > 0
       end
     end
+    
+    def mock_website_wrapper(html_files, wishlist_id = "WISHLIST-ID", email = "address@email.com") 
+      mock_wrapper = mock(Services::WebsiteWrapper)
+      mock_wrapper.should_receive(:find_for).with(email).and_return([{:id=>wishlist_id}]) 
+      html_files.each_with_index do |f, i|
+        page_num = i+1
+        page = open(File.join(PROJECT_DIR, "samples","uk", f )).read
+        mock_wrapper.should_receive(:get_page).with( wishlist_id, page_num).and_return(page) 
+      end
+      mock_wrapper
+    end
+    
   end
 end
